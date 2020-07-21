0d6b5cb6ce6ed19be20604ca6a6bef68edf69eb7
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-07-21 17:11:16 +0200

    add submenue

--- js/main.js
+++ js/main.js
@@ -21,6 +21,37 @@ $(document).ready(function () {
     }
   });
 
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 250) {
+      $('.sub_div').css('top', '35px');
+    } else {
+      $('.sub_div').css('top', '-120px');
+    }
+  });
+
+  // submenu
+  $('.main_li').hover(function () {
+    if ($(this).children('a').text() !== 'Home') {
+      $(this).find('.sub_div').show();
+    }
+  }, function () {
+    var subMenu = $(this).find('.sub_div');
+    if ($(this).children('a').text() !== 'Home') {
+      subMenu.hide();
+    }
+  });
+
+  $('.sub_div').hover(function () {
+    $(this).show();
+  }, function () {
+    $(this).hide();
+  });
+
+
+
+
+
+
   // scroll up in mobile screens
   $(window).scroll(function () {
     if ($(this).scrollTop() >= 600) {
@@ -41,7 +72,7 @@ $(document).ready(function () {
   // show special offer popup after web is loading
   setTimeout(function () {
     specialPopup.fadeIn();
-  }, 8000);
+  }, 10000);
   // make popup disapper when click close icon
   $('.close_special_popup').click(function () {
     specialPopup.fadeOut();
@@ -55,7 +86,22 @@ $(document).ready(function () {
     }
   });
 
-
+  // video slider
+  $('.video_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: $('.video_prev'),
+    nextArrow: $('.video_next'),
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        draggable: true,
+      }
+    }, ]
+  });
 
   // image slider
   $('.slider').slick({
@@ -63,22 +109,53 @@ $(document).ready(function () {
     centerPadding: '260px',
     slidesToShow: 1,
     arrows: false,
+    draggable: false,
+    focusOnSelect: false,
     responsive: [{
       breakpoint: 767,
       settings: {
         arrows: false,
         centerMode: true,
         centerPadding: '40px',
-        slidesToShow: 1
+        slidesToShow: 1,
+        draggable: true,
       }
     }, ]
-
   });
 
-  // popup image slider
+
+
   $('.slider_popup').slick({
-    
+    prevArrow: $('.btn_prev'),
+    nextArrow: $('.btn_next')
   });
+
+  // slider in contactUs for Mobile only
+  $('.slider_images_info').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    autoplay: true,
+    prevArrow: false,
+    nextArrow: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+  });
+
+  // show popup slider
+  $('.center_items').click(function (e) {
+    e.stopPropagation();
+    // var currentAttr = $(this).find('img').attr("src");
+    $('.slider_popup').resize();
+    $('.slider_popup_section').show();
+  });
+
+  $('.close_popup').click(function () {
+    $('.slider_popup_section').hide();
+  });
+
+
 
   // Custom Prev Arrow
   $('.carousel_prev').click(function () {
@@ -98,13 +175,83 @@ $(document).ready(function () {
   });
 
   // handle red heart like
-  $('.heart_not_solid').click(function () {
+  $('.heart_not_solid').click(function (e) {
+    e.stopPropagation();
     $(this).hide();
     $(this).siblings('.heart_solid').show();
   });
-  $('.heart_solid').click(function () {
+  $('.heart_solid').click(function (e) {
+    e.stopPropagation();
     $(this).hide();
     $(this).siblings('.heart_not_solid').show();
+  });
+
+  // show share links
+  $('.share_icon').click(function (e) {
+    e.stopPropagation();
+    $(this).parent().hide();
+    $(this).parent().siblings('.social_share_links').show();
+  });
+  $('.social_share_links').hover(function () {
+    $(this).show();
+  }, function () {
+    var that = $(this);
+    var icons = $(this).siblings('.social_share_container');
+    setTimeout(function () {
+      that.hide();
+      icons.show();
+    }, 1200);
+  });
+
+  // like in slider popup
+
+  $('.heart_not_solid_popup').click(function () {
+    var likes = +$(this).siblings('.heart_solid_popup').find('.likes_number').text();
+    likes += 1;
+    console.log(likes);
+    $(this).hide();
+    $(this).siblings('.heart_solid_popup').show();
+    $(this).siblings('.heart_solid_popup').find('.likes_number').text(likes);
+  });
+  $('.heart_solid_popup').click(function () {
+    var likes = +$(this).find('.likes_number').text();
+    likes -= 1;
+    $(this).hide();
+    $(this).siblings('.heart_not_solid_popup').show();
+    $(this).find('.likes_number').text(likes);
+  });
+
+  //show share div icons
+  $('.share_icon_popup').hover(function () {
+    $('.share_controls_part').show();
+  }, function () {
+    $('.share_controls_part').hide();
+  });
+  // when hover share_controls_part
+  $('.share_controls_part').hover(function () {
+    $(this).show();
+  }, function () {
+    $(this).hide();
+  });
+
+  $('.slider_popup').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    var imageName = $('.slick-current').find('.img_slider_popup img').attr("src");
+    imageName = imageName.slice(4);
+    $('.image_name').text(imageName);
+  });
+
+  // video handle share icons
+
+  $('.vjs-big-play-button').html('<h2 class="mb-4">The Crown - King</h2><p class="v_player"><span class="icon"><i class="far fa-play-circle"></i></span><span class="text">Play Video</span></p>');
+
+  $('.bar_video').click(function () {
+    $(this).hide();
+    $(this).siblings('.controls_video_div').show();
+  });
+
+  $('.close_v_icon').click(function () {
+    $(this).parent().hide();
+    $(this).parent().siblings('.bar_video').show();
   });
 
 });
